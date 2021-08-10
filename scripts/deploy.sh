48b978ba8d985c601dfb0a5fc5858e89b64b88ec
Shudodog <eaglesj97@gmail.com>
2021-08-11 03:38:04 +0900

    travis codedeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,9 @@ echo "> Build 파일복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션  pid 확인"
+echo "> 현재 구동중인 애플리케이션  pid확인"
 
-CURRENT_PID=$(pgrep -fl shudodog-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl shudodog-springboot2-webservice | grep java | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
