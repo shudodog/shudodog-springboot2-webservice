120d3ab449b8f61101a47281bfe24a04438c3238
Shudodog <eaglesj97@gmail.com>
2021-08-11 02:34:15 +0900

    travis codedeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ CURRENT_PID=$(pgrep -fl shudodog-springboot2-webservice | grep jar | awk '{print
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
